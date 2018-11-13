8c9ec189d050660e224e1a737429687383c4b2d0
sarahbcuevas <sarahbcuevas@gmail.com>
2018-11-14 04:59:39 +0800

    Updated mongoUrl

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     // 'mongoUrl': 'mongodb://localhost:27017'
-    'mongoUrl': 'mongodb://sbcuevas:sbcuevas1@ds041218.mlab.com:41218/heroku_nxzh92pw'
+    'mongoUrl': 'mongodb://heroku_nxzh92pw:uqkohp7cis5dcins3r3caashpi@ds041218.mlab.com:41218/heroku_nxzh92pw'
 };
