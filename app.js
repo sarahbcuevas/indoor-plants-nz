60fa2d656697456581e858ee9a1ca1835a12c90b
sarahbcuevas <sarahbcuevas@gmail.com>
2018-11-14 12:53:48 +0800

    Updated corsOptions

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // CORS config
 var corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:4200',
   // origin: 'http://1b4e6389.ngrok.io',
   // origin: 'http://192.168.1.26:4200',
   // origin: 'http://192.168.0.88:4200',
