ac10080cad93578a9d211a1a7ea5a6ac77237e31
sarahbcuevas <sarahbcuevas@gmail.com>
2018-12-13 22:23:34 +0800

    Added 'Access-Control-Allow-Origin' to cors headers

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ var corsOptions = {
   optionsSuccessStatus: 200,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'x-access-token', 'Authorization']
+  allowedHeaders: ['Content-Type', 'x-access-token', 'Authorization', 'Access-Control-Allow-Origin']
 };
 app.use(cors(corsOptions));
 
