2b15b9439882df4595bdc5bda01059431b07079a
Sarah Cuevas <sarahbcuevas@gmail.com>
2021-09-14 14:22:01 +0800

    Slideshow Feature

--- models/content.js
+++ models/content.js
@@ -1,6 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var item = new Schema({
+    image: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    url: {
+        type: String
+    },
+    action: {
+        type: String
+    }
+});
+
 var contentSchema = new Schema({
     shopName: {
         type: String,
@@ -14,18 +33,7 @@ var contentSchema = new Schema({
         type: String,
         required: false
     },
-    jumbotronImage: {
-        type: String,
-        required: false
-    },
-    jumbotronTitle: {
-        type: String,
-        required: false
-    },
-    jumbotronDescription: {
-        type: String,
-        required: false
-    },
+    slideshow: [item],
     footerDescription: {
         type: String,
         required: true
